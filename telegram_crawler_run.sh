03d21754cd71835719b8ae89f30525407fd44fdd
Sakura-501 <2359216537@qq.com>
2024-01-06 15:47:01 +0800

    readme

--- telegram_crawler_run.sh
+++ telegram_crawler_run.sh
@@ -4,19 +4,19 @@ pkill -f "telegram_crawler_run.sh"
 while [ 1 ]; do
     #nohup command > output.log 2>&1 &
     echo "1. 首先获取99个keywords"
-    python ${PWD}/main.py keywords
+    python3 ${PWD}/main.py keywords
     sleep 5;
     echo "2.1 从数据库获取关键字（默认99个），去查询group/channel"
-    python ${PWD}/main.py search_group_channel from_table
+    python3 ${PWD}/main.py search_group_channel from_table
     sleep 5;
     echo "2.2 从config.ini获取关键字（需自行添加），去查询group/channel"
-    python ${PWD}/main.py search_group_channel from_config
+    python3 ${PWD}/main.py search_group_channel from_config
     sleep 5;
     echo "3. 从数据库获取group/channel，查询历史消息（默认200个，每个1000条）"
-    nohup python ${PWD}/main.py history_message > telegram_crawler.log 2>&1 &
+    nohup python3 ${PWD}/main.py history_message > telegram_crawler.log 2>&1 &
     # 12个小时
     sleep 43200;
     echo "4. 从数据库获取历史消息，过滤出外链（默认20w条）"
-    python ${PWD}/main.py external_links
+    python3 ${PWD}/main.py external_links
     sleep 5;
 done
