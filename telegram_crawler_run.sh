f92924d70ee1e36a9e34e582efadd3749c5e0ef1
Sakura-501 <2359216537@qq.com>
2024-01-07 21:00:01 +0800

    sh

--- telegram_crawler_run.sh
+++ telegram_crawler_run.sh
@@ -3,6 +3,9 @@ echo "Stop last telegram_crawler_run.sh first, Please wait for a moment!"
 pkill -f "telegram_crawler_run.sh"
 while [ 1 ]; do
     #nohup command > output.log 2>&1 &
+    echo "4. 从数据库获取历史消息，过滤出外链（默认200w条，鉴于第三步耗时过长，将第四步提前进行）"
+    python3 telbot -e from_collection
+    sleep 5;
     echo "1. 首先获取99个keywords"
     python3 telbot -k from_config
     sleep 5;
