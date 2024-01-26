e125c63fd6f3143bf24eb2a75b5a0a624daf771a
Sakura-501 <2359216537@qq.com>
2024-01-26 15:30:51 +0800

    sh更改

--- telegram_crawler_run.sh
+++ telegram_crawler_run.sh
@@ -6,18 +6,28 @@ while [ 1 ]; do
     echo "4. 从数据库获取历史消息，过滤出外链（默认200w条，鉴于第三步耗时过长，将第四步提前进行）"
     python3 telbot -e from_collection
     sleep 5;
+
     echo "1. 首先获取99个keywords"
     python3 telbot -k from_config
     sleep 5;
+
     echo "2.1 从数据库获取关键字（默认99个），去查询group/channel"
     python3 telbot -s from_collection
     sleep 5;
+
     echo "2.2 从config.ini获取关键字（需自行添加），去查询group/channel"
     python3 telbot -s from_config
     sleep 5;
-    echo "3. 从数据库获取group/channel，查询历史消息（默认200个，每个1000条）"
+
+    echo "3.1 从数据库获取group/channel，查询历史消息（默认200个，每个10000条）"
     python3 telbot -m from_collection
+    sleep 5;
+
+#    echo "3.2 从config.ini文件获取group/channel，查询历史消息（默认200个，每个10000条）"
+#    python3 telbot -m from_config
+#    sleep 5;
+
 #     12个小时
 #    sleep 43200;
-    sleep 21600;
+#    sleep 21600;
 done
